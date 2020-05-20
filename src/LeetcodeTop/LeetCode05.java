20918c30fbf6907fe955d7a3ec909a2ad1c7e576
birdguan <xuchen_guan@163.com>
2020-05-20 23:10:12 +0800

    Update

---
+++ src/LeetcodeTop/LeetCode05.java
@@ -0,0 +1,34 @@
+package LeetcodeTop;
+
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ */
+public class LeetCode05 {
+    /**
+     * 中心扩展法
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i+1);
+            int len = Math.max(len1, len2);
+            if (len > end - start + 1) {
+                start = i -  (len - 1)/2;
+                end = i + len/2;
+            }
+        }
+        return s.substring(start, end+1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}

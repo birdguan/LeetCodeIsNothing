867dc43799c565212e3f2ab97dd15e78b0f9ce9b
11118500 <46611253@qq.com>
2020-05-25 13:22:51 +0800

    update

---
+++ src/LeetcodeTop/LeetCode20.java
@@ -0,0 +1,32 @@
+package LeetcodeTop;
+
+/**
+ * @Author: birdguan
+ * @Date: 2020/5/25 13:05
+ */
+
+/**
+ * LeetCode20 有效的括号 EASY
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode20 {
+
+    /**
+     * 使用栈
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+
+    }
+}

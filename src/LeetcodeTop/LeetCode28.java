0c6ca9799c8e5247925ac1590c3d44360dcbde9f
birdguan <xuchen_guan@163.com>
2020-05-31 11:14:28 +0800

    Update

--- src/LeetcodeTop/LeetCode28.java
+++ src/LeetcodeTop/LeetCode28.java
@@ -25,5 +25,46 @@ package LeetcodeTop;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class LeetCode28 {
-    
+    /**
+     * 方法一：子串注意比较——线性时间复杂度
+     * 时间复杂度：O((N-L)L)
+     * 空间复杂度：O(1)
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStrSolution1(String haystack, String needle) {
+        int l = needle.length(), n = haystack.length();
+        for (int start = 0; start <= n - l; start++) {
+            if (haystack.substring(start, start + l).equals(needle)) {
+                return start;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 方法二：双指针——线性时间复杂度
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStrSolution2(String haystack, String needle) {
+        int l = needle.length(), n = haystack.length();
+        if (l == 0) return 0;
+        int pn = 0;
+        while (pn < pn - l + 1) {
+            while (pn < n - l + 1 && haystack.charAt(pn) != needle.charAt(0)) pn++;
+            int currLen = 0, pl = 0;
+            while (pl < l && pn < n && haystack.charAt(pn) == needle.charAt(pl)) {
+                pn++;
+                pl++;
+                currLen++;
+            }
+            if (currLen == l) return pn-l;
+            // pn回溯
+            pn = pn - currLen + 1;
+        }
+        return -1;
+    }
 }
---
+++ src/LeetcodeTop/LeetCode29.java
@@ -0,0 +1,50 @@
+package LeetcodeTop;
+
+/**
+ * @Author: birdguan
+ * @Date: 2020/5/30 21:47
+ */
+
+/**
+ * LeetCode29 两数相除 MEDIUM
+ * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ *
+ * 返回被除数 dividend 除以除数 divisor 得到的商。
+ *
+ * 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/divide-two-integers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode29 {
+    /**
+     * 左移，正数边界问题比较麻烦，改用负数计算
+     * @param dividend
+     * @param divisor
+     * @return
+     */
+    public int divide(int dividend, int divisor) {
+        boolean sign = (dividend > 0) ^ (divisor > 0);
+        int result = 0;
+        // 改用负数处理
+        if (dividend > 0) dividend = -dividend;
+        if (divisor > 0) divisor = -divisor;
+        while (dividend <= divisor) {
+            int temp_result = -1;
+            int temp_divisor = divisor;
+            while (dividend <= (temp_divisor << 1)) {
+                if (temp_divisor <= (Integer.MIN_VALUE >> 1)) break;
+                temp_result = temp_result << 1;
+                temp_divisor = temp_divisor << 1;
+            }
+            dividend -= temp_divisor;
+            result += temp_result;
+        }
+        if (!sign) {
+            if (result <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            result = -result;
+        }
+        return result;
+    }
+}

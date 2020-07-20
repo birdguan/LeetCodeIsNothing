20cadb4da8ffa4363c6d479e851b8d7a666a44ca
birdguan <xuchen_guan@163.com>
2020-07-20 17:32:18 +0800

    Update

---
+++ src/OfferSword/Sword62.java
@@ -0,0 +1,43 @@
+package OfferSword;
+
+/**
+ * @Author: birdguan
+ * @Date: 2020/7/20 17:14
+ */
+
+/**
+ * Sword62 圆圈中最后剩下的数字 EASY
+ * 0,1,,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字。求出这个圆圈里剩下的最后一个数字。
+ *
+ * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入: n = 5, m = 3
+ * 输出: 3
+ * 示例 2：
+ *
+ * 输入: n = 10, m = 17
+ * 输出: 2
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Sword62 {
+    /**
+     * 数学方法：约瑟夫环问题
+     * @param n 数字范围
+     * @param m 删除位置
+     * @return
+     */
+    public int lastRemaining(int n, int m) {
+        int res = 0;
+        for (int i = 2; i <= n; i++) {
+            res = (res + m) % i;
+        }
+        return res;
+    }
+}
---
+++ src/OfferSword/Sword65.java
@@ -0,0 +1,38 @@
+package OfferSword;
+
+/**
+ * @Author: birdguan
+ * @Date: 2020/7/20 17:28
+ */
+
+/**
+ * Sword65 不用加减乘除做加法 EASY
+ * 写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/” 四则运算符号。
+ *
+ *  
+ *
+ * 示例:
+ *
+ * 输入: a = 1, b = 1
+ * 输出: 2
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Sword65 {
+    /**
+     * 位运算
+     * @param a 加数
+     * @param b 被加数
+     * @return 和
+     */
+    public int add(int a, int b) {
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a ^= b;
+            b = carry;
+        }
+        return a;
+    }
+}

baa8d81d7db04b09de6a60cdad7953876c8d96ba
birdguan <xuchen_guan@163.com>
2020-08-01 13:44:30 +0800

    Update

---
+++ src/OfferSword/Sword37.java
@@ -0,0 +1,86 @@
+package OfferSword;
+
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Sword37 序列化二叉树 HARD
+ * 请实现两个函数，分别用来序列化和反序列化二叉树。
+ *
+ * 示例: 
+ *
+ * 你可以将以下二叉树：
+ *
+ *     1
+ *    / \
+ *   2   3
+ *      / \
+ *     4   5
+ *
+ * 序列化为 "[1,2,3,null,null,4,5]"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/xu-lie-hua-er-cha-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Sword37 {
+    class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * BFS
+     * @param root 二叉树根结点
+     * @return 序列化后的字符串
+     */
+    public String serialize(TreeNode root) {
+        if (root == null) return "[]";
+        StringBuilder res = new StringBuilder("[");
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                res.append(node.val).append(",");
+                queue.offer(node.left);
+                queue.offer(node.right);
+            } else {
+                res.append("null,");
+            }
+        }
+        res.deleteCharAt(res.length() - 1);
+        res.append("]");
+        return res.toString();
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data.equals("[]")) return null;
+        String[] vals = data.substring(1, data.length()-1).split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (!vals[i].equals("null")) {
+                node.left = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.left);
+            }
+            i++;
+            if (!vals[i].equals("null")) {
+                node.right = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.right);
+            }
+            i++;
+        }
+        return root;
+    }
+}

cc385ff0e86f8063efe714114a4f475ca0f342f7
birdguan <xuchen_guan@163.com>
2020-08-04 22:06:06 +0800

    Update

--- src/OfferSword/Sword41.java
+++ src/OfferSword/Sword41.java
@@ -1,5 +1,8 @@
 package OfferSword;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * Sword41 数据流中的中位数 HARD
  * 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
@@ -32,4 +35,23 @@ package OfferSword;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Sword41 {
+    Queue<Integer> A, B;
+    public Sword41() {
+        A = new PriorityQueue<>();      // 小顶堆，保存较大的一半
+        B = new PriorityQueue<>((x, y) -> (y - x)); // 大顶堆，保存较小的一半
+    }
+
+    public void addNum(int num) {
+        if (A.size() != B.size()) {
+            A.add(num);
+            B.add(A.poll());
+        } else {
+            B.add(num);
+            A.add(B.poll());
+        }
+    }
+
+    public double findMedian() {
+        return A.size() != B.size() ? A.peek() : (A.peek() + B.peek()) / 2.0;
+    }
 }
